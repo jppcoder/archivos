8949e8d561798ecab083d73d6e7aa54e09e691eb
Juan Pablo Pagotto <jppcoder@gmail.com>
2021-10-12 08:01:02 -0300

    Update main.js
    pruebas con manejo de archivo

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
+   
 const fs = require('fs');
 
-  class Contenedor {
-      constructor(productos) {
+//let texto = ("probando");
 
-      
-    async function escribir(texto) {
+let objtest  = {
+    siteName: "W3Docs",
+    bookName: "Javascript",
+    booksCount: 5
+}
 
-              try {
-                  await fs.promises.writeFile('/Users/juanpablopagotto/backend/segundo/prueba.txt', `${texto}`);
-                  console.log('guardado');
+let objprueba = ("");
 
-              } catch (error) {
-                  console.log('no pude guardarlo');
-              }
-          }
+function makeJson(objtest, objprueba) {  
+    objprueba = JSON.stringify(objtest);
+    return objprueba
+}
+
+const count = [0];
+
+async function save(objtest, count) {
+
+    try {
+        
+        await fs.promises.writeFile('texto.txt', JSON.stringify(objtest));
+        await fs.promises.writeFile('contador.txt',  "numero 1");
+        console.log('guardado');
+
+    } catch (error) {
+        console.log('no pude guardarlo');
     }
 }
+
+save(objtest)
+
+
+/*async function escribir(texto) {
+
+    try {
+        await fs.promises.writeFile('texto.txt', `${texto}`);
+        console.log('guardado');
+
+    } catch (error) {
+        console.log('no pude guardarlo');
+    }
+}
+*/
